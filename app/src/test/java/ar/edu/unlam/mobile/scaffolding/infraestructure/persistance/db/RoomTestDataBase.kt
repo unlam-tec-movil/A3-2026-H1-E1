647c8c9af009206df92652743612c0d419e18668
Kajukai32 <arturojas32@gmail.com>
2026-06-09 22:18:28 -0300

    chore: remove unused mockk import in RoomTestDataBase

--- app/src/test/java/ar/edu/unlam/mobile/scaffolding/infraestructure/persistance/db/RoomTestDataBase.kt
+++ app/src/test/java/ar/edu/unlam/mobile/scaffolding/infraestructure/persistance/db/RoomTestDataBase.kt
@@ -2,7 +2,6 @@ package ar.edu.unlam.mobile.scaffolding.infraestructure.persistance.db
 
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import org.robolectric.RuntimeEnvironment
 
